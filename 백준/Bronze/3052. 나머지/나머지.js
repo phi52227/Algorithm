ecb4bc4dc5adf6b8b930989efcaf12be46204deb
hyein <73422726+phi52227@users.noreply.github.com>
2023-07-28 10:21:34 +0900

    [Bronze II] Title: 나머지, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 var input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
-input = input.map(x => x % 42);
-let set = new Set(input);
+let set = new Set(input.map(x => x % 42));
 let arr = [...set];
 console.log(arr.length);
